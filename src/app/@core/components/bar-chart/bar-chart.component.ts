c3254deaa2d7c4354715639bb4d5fdf669edd8d8
Anartz Mugika Ledo <mugan86@gmail.com>
2019-08-07 19:46:00 +0200

    Video 16 OK

--- src/app/@core/components/bar-chart/bar-chart.component.ts
+++ src/app/@core/components/bar-chart/bar-chart.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { Label } from 'ng2-charts';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
   styleUrls: ['./bar-chart.component.css']
 })
-export class BarChartComponent implements OnInit {
-
+export class BarChartComponent implements OnInit, OnChanges {
+  @Input() characters: Character[];
   public barChartOptions: ChartOptions = {
     responsive: true,
     // We use these empty structures as placeholders for dynamic theming.
@@ -20,12 +21,13 @@ export class BarChartComponent implements OnInit {
       }
     }
   };
-  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  labelChart = 'Personajes Breaking Bad';
+  public barChartLabels: Label[] = [];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
 
   public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Breaking Bad votaciones personajes' },
+    { data: [], label: this.labelChart },
   ];
 
   public colors = [
@@ -37,6 +39,22 @@ export class BarChartComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: any) {
+    console.log(this.characters);
+    this.loadChartData();
+  }
+
+  loadChartData() {
+    this.barChartLabels = [];
+    this.barChartData = [
+      { data: [], label: this.labelChart },
+    ];
+    this.characters.map((item: Character) => {
+      this.barChartLabels.push(item.name);
+      this.barChartData[0].data.push(item.votes);
+    });
   }
 
   // events
--- src/app/@pages/votes/votes.component.ts
+++ src/app/@pages/votes/votes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from 'src/app/@core/interfaces/character.interface';
+import { ApiService } from 'src/app/@core/services/api.service';
 
 @Component({
   selector: 'app-votes',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./votes.component.css']
 })
 export class VotesComponent implements OnInit {
-
-  constructor() { }
+  characters: Character[] = [];
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    this.apiService.getCharacters(true).subscribe((data) => {
+      this.characters = data;
+      console.log(this.characters);
+    });
   }
 
 }
